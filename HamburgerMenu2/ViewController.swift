d61e5877f4497bb4a33d9ed30ab87bb42f70ba01
“Tushar <tushar.kolhe01@gmail.com>
2018-07-09 23:35:01 +0530

    Initial commit

---
+++ HamburgerMenu2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  HamburgerMenu2
+//
+//  Created by Tushar Kolhe on 09/07/18.
+//  Copyright © 2018 Tushar Kolhe. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
